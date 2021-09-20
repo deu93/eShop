475e02727217d9f0e5da8fe484f89e54043d3c9c
deu93 <deucalion93@ukr.net>
2021-09-20 10:52:21 +0300

    delete category complete

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -74,5 +74,17 @@ class CategoryController extends Controller
 
         return redirect('/dashboard')->with('status', 'Category Updated Successfully');
     }
+
+    public function destroy($id) {
+        $category = Category::find($id);
+        if($category->image) {
+            $path = 'assets/uploads/category/' . $category->image;
+            if(File::exists($path)) {
+                File::delete($path);
+            }
+        }
+        $category->delete();
+        return redirect('/categories')->with('status', 'Category Deleted Successfully');
+    }
 }
 
