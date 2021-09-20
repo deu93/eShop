88bcbc03a2b27f59f847f8a1b9e7443f22d05181
deu93 <deucalion93@ukr.net>
2021-09-20 09:23:15 +0300

    edit category complete

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\admin;
 
-use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
 
 class CategoryController extends Controller
 {
@@ -18,9 +19,6 @@ class CategoryController extends Controller
     }
 
     public function insert(Request $request) {
-       
-       
-       
        
         $category = new Category();
         if($request->hasFile('image')){
@@ -41,6 +39,40 @@ class CategoryController extends Controller
         $category->meta_description = $request->input('meta_description');
         $category->save();
         return redirect('/dashboard')->with('status', 'Category Added Successfully');
+    }
+
+    public function edit($id) {
+        $category = Category::find($id);
+        return view('admin.category.edit', compact('category'));
+    }
+
+    public function update(Request $request, $id) {
+        $category = Category::find($id);
+        if($request->hasFile('image')) {
+            $path = 'assets/uploads/category/' . $category->image;
+            if(File::exists($path)) {
+                File::delete($path);
+            }
+
+            $file = $request->file('image');
+            $ext = $file->getClientOriginalExtension();
+            $filename = time() . '.' . $ext;
+            $file->move('assets/uploads/category', $filename);
+            $category->image = $filename;
+
+        }
+
+        $category->name = $request->input('name');
+        $category->slug = $request->input('slug');
+        $category->description = $request->input('description');
+        $category->status = $request->input('status') == TRUE ? '1':'0';
+        $category->popular = $request->input('popular') == TRUE ? '1':'0';
+        $category->meta_title = $request->input('meta_title');
+        $category->meta_keywords = $request->input('meta_keywords');
+        $category->meta_description = $request->input('meta_description');
+        $category->update();
+
+        return redirect('/dashboard')->with('status', 'Category Updated Successfully');
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,6 @@ Route::middleware(['auth','isAdmin'])->group(function(){
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::get('/add-category', [CategoryController::class, 'add']);
     Route::post('/insert-category', [CategoryController::class, 'insert']);
-    Route::get('edit-prod', [CategoryController::class, 'edit']);
+    Route::get('/edit-prod/{id}', [CategoryController::class, 'edit']);
+    Route::put('/update-category/{id}', [CategoryController::class, 'update']);
 });
